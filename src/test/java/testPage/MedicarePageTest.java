635d4965f129b558f5b62e402df42ff463e49215
Preethy87999 <124979741+Preethy87999@users.noreply.github.com>
2023-09-04 20:16:32 +0530

    Update MedicarePageTest.javamm

--- src/test/java/testPage/MedicarePageTest.java
+++ src/test/java/testPage/MedicarePageTest.java
@@ -38,7 +38,8 @@ public class MedicarePageTest {
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("http://localhost:8081/medicare/");	
+		// driver.get("http://localhost:8081/medicare/");	
+		driver.get("http://localhost:8080/medicare1/");	
 	}
 	
 	@Test(priority =1)
